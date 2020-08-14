aa43677ca28bd0900ce5e075b46d9330706b1caa
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2020-08-14 08:50:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+<h4>Linux<h4> 
+
+### Python coding ###
+
+This program can attack website.This ping attack is impact your target website.I am not responsible when you use it illegal purpose.It is only educational purpose.
---
+++ ping-attack.py
@@ -0,0 +1,17 @@
+import os
+import sys
+os.system("clear")
+os.system( "toilet -f mono9 -F gay Ping Attack")
+print
+print      'Author  :  Rahat Khan Tusar(RKT)'
+print  
+print 'Information  :  Use vpn/openvpn/tor/tor proxy network.Be anonymous.'
+print
+ip = raw_input("Target Url/Ip :")
+print
+t = input("Set Amount :")
+print
+c = input("Set Count :")
+print
+os.system("ping  {}  ".format(ip,t,c))
+
